c9db1620bb209d501f11ab520ab07c1e567a1b1e
Siddharth Kumar Jha <siddharthkjha6909@gmail.com>
2023-04-03 22:30:19 +0530

    Partially working User page

---
+++ src/pages/Sidebar.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import cimg from './aurionpro-Logo_Color-1-01_Aurionpro-Logo-2022-color-black.svg';
+
+function Menuitems(props){
+  return(
+    <div>
+      {props.data.map((val, key) => {
+        return(
+          <a href="#" key={key}>
+            <span className='icon'>
+              <i className={val.iconclass}></i>
+            </span>
+            {val.msg}
+          </a>
+        )
+      })}
+    </div>
+  );
+}
+
+function PopUpMenuitems(props) {
+  return (
+    <a href="#">
+      <span className='icon'>
+        <i className={props.data.iconclass}></i>
+      </span>
+      {props.data.msg}
+    </a>
+  );
+}
+function Menu(props) {
+  return(
+    <div className="menu">
+      <Menuitems data={props.menudata} />
+    </div>
+  );
+}
+
+function PopUpMenu(){
+  const menudata = [
+    {msg: "logout", iconclass: "ri-logout-box-r-line"},
+    {msg: "Profile Settings", iconclass: "ri-user-settings-line"},
+  ];
+  return(
+    <ul className="drop--down">
+      <li>
+        <PopUpMenuitems data={menudata[0]} />
+      </li>
+      <li>
+        <PopUpMenuitems data={menudata[1]} />
+      </li>
+    </ul>
+  );
+}
+
+function Profile(props){
+  const [popUpMenu, setPopUpMenu] = React.useState(false);
+  return(
+    <div>
+      {popUpMenu && PopUpMenu()}
+      <button className="profile" onClick={ () => setPopUpMenu(!popUpMenu) } >
+          <div className="profile-img">
+              <img src={props.img} alt="profile" />
+          </div>
+          <div className="name">
+              <h1>{props.profiledata.name}</h1>
+              <p>{props.profiledata.stat}</p>
+          </div>
+      </button>
+    </div>
+  );
+}
+function Sidebar(props) {
+  return(
+    <div className="sidebar">
+      <a href="#" className="logo">
+        <img src={cimg} alt="logo"></img>
+      </a>
+      <Menu menudata={props.menudata} />
+      <Profile profiledata={props.profiledata[0]} img={props.img} />
+    </div>
+  );
+}
+export default Sidebar;
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import Sidebar from './Sidebar';
+import Table from './Table';
+import logo from './logo.svg'
 import './user.css';
 
-class User extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function MainHome(props){
+  return(
+    <div className='main--home'>
+      <Table tabledata={props.tabledata} />
+    </div>
+  );
+}
+
+function User() {
+
+  const menudata = [
+    {msg: 'Queues', iconclass: "ri-dashboard-line"},
+    {msg: 'Customers', iconclass: "ri-team-fill"},
+    {msg: 'Reports', iconclass: "ri-file-chart-line"},
+    {msg: 'Raise a request', iconclass: "ri-inbox-unarchive-line"},
+  ];
+
+  const profiledata = [
+    {name: "Siddharth Kumar Jha", stat: "Employee"}
+  ];
+  
+  const tabledata = [
+    {title: "Products are not working, pls help", priority: "Urgent", category: "Product", type: "Bug", assignto: "Peeves", stat: "In Progress", created: "23/Jul/23"},
+    {title: "Users cannot login their account", priority: "Urgent", category: "system", type: "Bug", assignto: "", stat: "In Progress", created: "23/Jul/23"},
+    {title: "Campaign emails are not receiving the users", priority: "Important", category: "Emails", type: "Bug", assignto: "House-elves teams", stat: "In Progress", created: "23/Jul/23"},
+    {title: "Some Ideas to improve the new product", priority: "Nice to do", category: "Product", type: "Feedback", assignto: "Siddharth Kumar Jha (me)", stat: "In Progress", created: "22/Jul/23"},
+  ];
+
+  return (
+    <div className="container-new">
+      <Sidebar menudata={menudata} profiledata={profiledata} img={logo}/>
+      <MainHome tabledata={tabledata}/>
+    </div>
+  );
+
 }
 
 export default User;
