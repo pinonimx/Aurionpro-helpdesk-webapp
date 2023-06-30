f3ca88e8a40cacfea396728cfd0509b55873b617
Siddharth Kumar Jha <siddharthkjha6909@gmail.com>
2023-06-30 13:32:46 +0530

    removing dev files

--- mysql/insertProjectType.sql
+++
@@ -1,5 +0,0 @@
-use aurionpro;
-insert into projecttype set `type` = 'KCC-ISS';
-insert into projecttype set `type` = 'PMFBY';
-insert into projecttype set `type` = 'CCE';
-select * from projecttype;
